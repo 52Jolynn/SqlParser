d1d1d6e3ec5ae17d6e0ecb0c9d3a2f9a9eeb74ac
huangtiande <htd0324@gmail.com>
2014-05-07 15:43:15 +0800

    新増语法文件

--- src/main/java/com/laudandjolynn/sqlparser/SqlParser.java
+++ src/main/java/com/laudandjolynn/sqlparser/SqlParser.java
@@ -1,5 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 htd0324@gmail.com.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     htd0324@gmail.com - initial API and implementation
+ ******************************************************************************/
 package com.laudandjolynn.sqlparser;
 
+/**
+ * 
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2014年5月7日 上午9:24:00
+ * @copyright: www.laudandjolynn.com
+ */
 public class SqlParser {
 
 }
--- src/test/java/com/laudandjolynn/sqlparser/SqlParserTest.java
+++ src/test/java/com/laudandjolynn/sqlparser/SqlParserTest.java
@@ -1,7 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2014 htd0324@gmail.com.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     htd0324@gmail.com - initial API and implementation
+ ******************************************************************************/
 package com.laudandjolynn.sqlparser;
 
 import junit.framework.TestCase;
 
+/**
+ * 
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2014年5月7日 上午9:24:00
+ * @copyright: www.laudandjolynn.com
+ */
 public class SqlParserTest extends TestCase {
 
 }
